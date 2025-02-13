ea72acefb5d6eca8e602f9f0000925b3f534c1e6
paolucciFrota <you@example.com>
2025-02-13 13:57:56 -0300

    //Content: Dia 1 - Começando pelo começo

---
+++ dia 1/começando pelo começo/introduction.js
@@ -0,0 +1,1 @@
+console.log('Bem-vindo a 30 dias tentando aprender JavaScript')
---
+++ dia 1/começando pelo começo/variable.js
@@ -0,0 +1,14 @@
+let firstName = 'Gabriel'
+let lastName = 'Correa'
+let country = 'Brasil'
+let city = 'Manaus'
+let age = 20
+let isMarried = false // :(
+
+const gravity = 9.81
+const boilingPoint = 100
+const PI = 3.14
+
+let name = 'Gabriel',
+    job = 'Developer',
+    live = 'Brasil'
